226219bd8d5573236e71d111eddfca54bea7bd7d
Misha Topol <misha.topol@gmail.com>
2020-10-21 00:06:29 +0300

    init code

---
+++ src/Client.php
@@ -0,0 +1,22 @@
+<?php
+namespace Letrams\SkupkaClient;
+
+class Client 
+{
+	public function send($data,$key)
+	{
+		$ch = curl_init('http://127.0.0.1:8000/api/v1/blago/new-lead');
+		
+		$authorization = 'Authorization: Bearer ' . $key;
+		curl_setopt($ch, CURLOPT_HTTPHEADER, [$authorization]);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data); 
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		echo $responce = curl_exec($ch);
+		curl_close($ch);	
+
+		return json_decode($responce);
+	}
+}
